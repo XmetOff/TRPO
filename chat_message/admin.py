3e11e0536be69fda5c962ff61ba0abdf21f6ed3a
serjlaren <serjlar@mail.ru>
2016-11-04 15:28:00 +0700

    chat_messages changed

---
+++ chat_message/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from chat_message.models import Message
+
+
+class MessageInline(admin.TabularInline):
+    model = Message
+
+
+admin.site.register(Message)
+
--- chat_message/forms.py
+++ chat_message/forms.py
@@ -1,21 +1,21 @@
 from django import forms
 
-from chatMessages.models import ChatMessage
+from chat_message.models import Message
 
 
 class MessageForm(forms.ModelForm):
     class Meta:
         fields = ['text']
-        model = ChatMessage
+        model = Message
 
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user', None)
-        self.chat = kwargs.pop('chat', None)
+        self.room = kwargs.pop('room', None)
         super().__init__(*args, **kwargs)
 
     def save(self, commit=True):
         message = super().save(commit=False)
         message.author = self.user
-        message.chat = self.chat
+        message.room = self.room
         message.save()
         return message
