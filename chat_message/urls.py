d66c695ffd9e99f6d63438931761d8cd61baca50
serjlaren <serjlar@mail.ru>
2016-11-08 05:51:56 +0700

    urls

--- chat_message/urls.py
+++ chat_message/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 
-from chatMessages.views import CreateMessageView
+from chat_message.views import CreateMessageView
 
 urlpatterns = [
-    url('^(?P<chat>\d+)', CreateMessageView.as_view(), name='create_message')
+    url(r'^(?P<room>\d)', CreateMessageView.as_view(), name='create_message')
 ]
