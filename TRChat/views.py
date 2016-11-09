66adf56838d4fb2f592f92a450a8c1d0592036f7
val118 <vllb1994@mail.ru>
2016-11-10 02:01:42 +0700

    TRChat views

---
+++ TRChat/views.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.views.generic.base import TemplateView
+
+
+# Create your views here.
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        return context
