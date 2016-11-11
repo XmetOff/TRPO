207ac4080c93b4b3115f5c7ba636397f58cd3aab
val118 <vllb1994@mail.ru>
2016-11-11 14:26:18 +0700

    urls

--- TRChat/urls.py
+++ TRChat/urls.py
@@ -1,26 +1,7 @@
-"""сhat URL Configuration
+from __future__ import unicode_literals
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf import settings
-from django.conf.urls import url, include
-from django.conf.urls.static import static
-from django.contrib import admin
+from django.conf.urls import url
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^chat_room/', include('chat_room.urls')),
-    url(r'^messages/', include('chatMessages.urls')),
-    url(r'^api/', include('chatMessages.api')),
-]
+from .views import HomePageView
+
+urlpatterns = [url(r'^(?!.)', HomePageView.as_view(), name='home')]
