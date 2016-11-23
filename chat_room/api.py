28bff1311494b83ba6bbf2e00678f198e4bf3a10
alexhot <Khotinskij@yandex.ru>
2016-11-23 14:35:11 +0700

    apps fixed

--- chat_room/api.py
+++
@@ -1,7 +0,0 @@
-# from .views import CourierViewSet
-# from rest_framework.routers import DefaultRouter
-#
-# router = DefaultRouter()
-# orders_router = router.register(r'messages', CourierViewSet, base_name='couriers')
-#
-# urlpatterns = router.urls
